dcd02fb79e40cc492e371003a22605719fe30265
Johanot <67147458+SirJohanot@users.noreply.github.com>
2021-12-18 12:13:53 +0300

    reformatted code and added builders

--- src/main/java/com/epam/xmlparsing/parser/PlantHandler.java
+++ src/main/java/com/epam/xmlparsing/parser/PlantHandler.java
@@ -13,27 +13,27 @@ import java.util.List;
 
 public class PlantHandler extends DefaultHandler {
 
-    private static final String PLANTS="plants";
-    private static final String TREE="tree";
-    private static final String FLOWER="flower";
-    private static final String ID="id";
-    private static final String ORIGIN="origin";
-    private static final String NAME="name";
-    private static final String SOIL_TYPE="soilType";
-    private static final String GROWING_TIPS="growingTips";
-    private static final String NEEDED_TEMPERATURE="neededTemperature";
-    private static final String NEEDS_LIGHT="needsLight";
-    private static final String WEEKLY_REQUIRED_WATER="weeklyRequiredWater";
-    private static final String PETAL_NUMBER="petalNumber";
-    private static final String TRUNK_DIAMETER="trunkDiameter";
+    private static final String PLANTS = "plants";
+    private static final String TREE = "tree";
+    private static final String FLOWER = "flower";
+    private static final String ID = "id";
+    private static final String ORIGIN = "origin";
+    private static final String NAME = "name";
+    private static final String SOIL_TYPE = "soilType";
+    private static final String GROWING_TIPS = "growingTips";
+    private static final String NEEDED_TEMPERATURE = "neededTemperature";
+    private static final String NEEDS_LIGHT = "needsLight";
+    private static final String WEEKLY_REQUIRED_WATER = "weeklyRequiredWater";
+    private static final String PETAL_NUMBER = "petalNumber";
+    private static final String TRUNK_DIAMETER = "trunkDiameter";
 
-    List<Plant> plants;
-    StringBuilder bufferedString;
-    Tree bufferedTree;
-    Flower bufferedFlower;
-    Plant currentPlant;
+    private List<Plant> plants;
+    private StringBuilder bufferedString;
+    private Tree.TreeBuilder bufferedTreeBuilder;
+    private Flower.FlowerBuilder bufferedFlowerBuilder;
+    private Plant.PlantBuilder currentPlantBuilder;
 
-    public PlantHandler(){
+    public PlantHandler() {
     }
 
     @Override
@@ -42,105 +42,98 @@ public class PlantHandler extends DefaultHandler {
     }
 
     @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        switch(qName){
+    public void startElement(String uri, String localName, String elementName, Attributes attributes) throws SAXException {
+        switch (elementName) {
             case PLANTS:
-                plants=new ArrayList<>();
+                plants = new ArrayList<>();
                 break;
             case TREE:
-                bufferedTree=new Tree();
-                parseAttributes(bufferedTree, attributes);
-                currentPlant=bufferedTree;
+                bufferedTreeBuilder = new Tree.TreeBuilder();
+                parseAttributes(bufferedTreeBuilder, attributes);
+                currentPlantBuilder = bufferedTreeBuilder;
                 break;
             case FLOWER:
-                bufferedFlower=new Flower();
-                parseAttributes(bufferedFlower, attributes);
-                currentPlant=bufferedFlower;
+                bufferedFlowerBuilder = new Flower.FlowerBuilder();
+                parseAttributes(bufferedFlowerBuilder, attributes);
+                currentPlantBuilder = bufferedFlowerBuilder;
                 break;
-            case NAME:
-            case SOIL_TYPE:
-            case NEEDED_TEMPERATURE:
-            case NEEDS_LIGHT:
-            case WEEKLY_REQUIRED_WATER:
-            case PETAL_NUMBER:
-            case TRUNK_DIAMETER:
-                bufferedString=new StringBuilder();
+            default:
+                if (elementName.equals(NAME) || elementName.equals(SOIL_TYPE) || elementName.equals(NEEDED_TEMPERATURE) || elementName.equals(NEEDS_LIGHT) || elementName.equals(WEEKLY_REQUIRED_WATER) || elementName.equals(PETAL_NUMBER) || elementName.equals(TRUNK_DIAMETER)) {
+                    bufferedString = new StringBuilder();
+                }
                 break;
         }
     }
 
     @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException {
-        switch(qName){
+    public void endElement(String uri, String localName, String elementName) throws SAXException {
+        switch (elementName) {
             case TREE:
-               plants.add(bufferedTree);
+                plants.add(new Tree(bufferedTreeBuilder));
                 break;
             case FLOWER:
-                plants.add(bufferedFlower);
+                plants.add(new Flower(bufferedFlowerBuilder));
                 break;
             case NAME:
-                String name=bufferedString.toString();
-                currentPlant.setName(name);
+                String name = bufferedString.toString();
+                currentPlantBuilder.setName(name);
                 break;
             case SOIL_TYPE:
-                String soilTypeLine=bufferedString.toString();
-                Soil soilType=Soil.valueOf(soilTypeLine);
-                currentPlant.setSoilType(soilType);
+                String soilTypeLine = bufferedString.toString();
+                Soil soilType = Soil.valueOf(soilTypeLine);
+                currentPlantBuilder.setSoilType(soilType);
                 break;
             case NEEDED_TEMPERATURE:
-                String neededTemperatureLine=bufferedString.toString();
-                int neededTemperature=Integer.parseInt(neededTemperatureLine);
-                currentPlant.setNeededTemperature(neededTemperature);
+                String neededTemperatureLine = bufferedString.toString();
+                int neededTemperature = Integer.parseInt(neededTemperatureLine);
+                currentPlantBuilder.setNeededTemperature(neededTemperature);
                 break;
             case NEEDS_LIGHT:
-                String needsLightLine=bufferedString.toString();
-                boolean needsLight=Boolean.parseBoolean(needsLightLine);
-                currentPlant.setNeedsLight(needsLight);
+                String needsLightLine = bufferedString.toString();
+                boolean needsLight = Boolean.parseBoolean(needsLightLine);
+                currentPlantBuilder.setNeedsLight(needsLight);
                 break;
             case WEEKLY_REQUIRED_WATER:
-                String weeklyRequiredWaterLine=bufferedString.toString();
-                int weeklyRequiredWater=Integer.parseInt(weeklyRequiredWaterLine);
-                currentPlant.setWeeklyRequiredWater(weeklyRequiredWater);
+                String weeklyRequiredWaterLine = bufferedString.toString();
+                int weeklyRequiredWater = Integer.parseInt(weeklyRequiredWaterLine);
+                currentPlantBuilder.setWeeklyRequiredWater(weeklyRequiredWater);
                 break;
             case PETAL_NUMBER:
-                String petalNumberLine=bufferedString.toString();
-                int petalNumber=Integer.parseInt(petalNumberLine);
-                bufferedFlower.setPetalNumber(petalNumber);
+                String petalNumberLine = bufferedString.toString();
+                int petalNumber = Integer.parseInt(petalNumberLine);
+                bufferedFlowerBuilder.setPetalNumber(petalNumber);
                 break;
             case TRUNK_DIAMETER:
-                String trunkDiameterLine=bufferedString.toString();
-                double trunkDiameter=Double.parseDouble(trunkDiameterLine);
-                bufferedTree.setTrunkDiameter(trunkDiameter);
+                String trunkDiameterLine = bufferedString.toString();
+                double trunkDiameter = Double.parseDouble(trunkDiameterLine);
+                bufferedTreeBuilder.setTrunkDiameter(trunkDiameter);
                 break;
         }
     }
 
     @Override
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        if (bufferedString==null){
-            bufferedString=new StringBuilder();
-        } else{
-            bufferedString.append(ch, start, length);
+    public void characters(char[] characters, int start, int length) throws SAXException {
+        if (bufferedString == null) {
+            bufferedString = new StringBuilder();
+        } else {
+            bufferedString.append(characters, start, length);
         }
     }
 
-    private void parseAttributes(Plant destination, Attributes attributes){
-        for (int i=0;i<attributes.getLength();i++) {
-            switch (attributes.getLocalName(i)) {
-                case ID:
-                    String idLine=attributes.getValue(i);
-                    int id = Integer.parseInt(idLine);
-                    destination.setId(id);
-                    break;
-                case ORIGIN:
-                    String origin = attributes.getValue(i);
-                    destination.setOrigin(origin);
-                    break;
+    private void parseAttributes(Plant.PlantBuilder destination, Attributes attributes) {
+        for (int i = 0; i < attributes.getLength(); i++) {
+            if (attributes.getLocalName(i).equals(ID)) {
+                String idLine = attributes.getValue(i);
+                int id = Integer.parseInt(idLine);
+                destination.setId(id);
+            } else if (attributes.getLocalName(i).equals(ORIGIN)) {
+                String origin = attributes.getValue(i);
+                destination.setOrigin(origin);
             }
         }
     }
 
-    List<Plant> getPlants(){
+    List<Plant> getPlants() {
         return plants;
     }
 }
