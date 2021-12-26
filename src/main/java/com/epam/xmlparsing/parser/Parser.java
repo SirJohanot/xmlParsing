f60d991d020ca76df786f76a54216df3b16d74bc
Johanot <67147458+SirJohanot@users.noreply.github.com>
2021-12-26 18:11:35 +0300

    added new exceptions to method signature

--- src/main/java/com/epam/xmlparsing/parser/Parser.java
+++ src/main/java/com/epam/xmlparsing/parser/Parser.java
@@ -1,10 +1,15 @@
 package com.epam.xmlparsing.parser;
 
 import com.epam.xmlparsing.entity.Plant;
+import org.xml.sax.SAXException;
 
+import javax.xml.bind.JAXBException;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 public interface Parser {
 
-    List<Plant> parse(String filePath) throws ParserException;
+    List<Plant> parse(String filePath) throws ParserException, JAXBException, IOException, ParserConfigurationException, SAXException;
 }
