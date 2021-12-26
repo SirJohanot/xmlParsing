65298192d366f00a9aa95de54bbe7a1cbae51c8f
Johanot <67147458+SirJohanot@users.noreply.github.com>
2021-12-26 18:09:54 +0300

    moved variables to static final List field

--- src/test/java/com/epam/xmlparsing/parser/SaxParserTest.java
+++ src/test/java/com/epam/xmlparsing/parser/SaxParserTest.java
@@ -14,18 +14,19 @@ public class SaxParserTest {
 
     private static final SaxParser PARSER = new SaxParser();
     private static final String FILE_WITH_TWO_FLOWERS_AND_TWO_TREES = "src/test/resources/plants.xml";
+    private static final List<Plant> LIST_WITH_TWO_FLOWERS_AND_TWO_TREES = Arrays.asList(
+            new Flower(1, "", "Rose", Soil.CLAY, 20, true, 1500, 40),
+            new Flower(2, "Persia", "Dandelion", Soil.SILT, 15, true, 750, 50),
+            new Tree(3, "", "Oak", Soil.CLAY, 5, false, 560, 30.0),
+            new Tree(4, "North America", "Paper Birch", Soil.SAND, 5, true, 430, 40.0));
 
     @Test
-    public void testShouldReturnListOfPlantsWhenThereAreBothTreesAndFlowersInFile() throws ParserException {
+    public void testParseShouldReturnListOfPlantsWhenThereAreBothTreesAndFlowersInFile() throws ParserException {
         //given
-        Flower flower1 = new Flower(1, null, "Rose", Soil.CLAY, 20, true, 1500, 40);
-        Flower flower2 = new Flower(2, "Persia", "Dandelion", Soil.SILT, 15, true, 750, 50);
-        Tree tree1 = new Tree(3, null, "Oak", Soil.CLAY, 5, false, 560, 30.0);
-        Tree tree2 = new Tree(4, "North America", "Paper Birch", Soil.SAND, 5, true, 430, 40.0);
-        List<Plant> expectedList = Arrays.asList(flower1, flower2, tree1, tree2);
         //when
         List<Plant> actualList = PARSER.parse(FILE_WITH_TWO_FLOWERS_AND_TWO_TREES);
         //then
-        Assert.assertEquals(expectedList, actualList);
+        Assert.assertEquals(LIST_WITH_TWO_FLOWERS_AND_TWO_TREES, actualList);
     }
+
 }
