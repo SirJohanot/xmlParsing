753f44249832d76def8140f9531258e932d54bb6
Johanot <67147458+SirJohanot@users.noreply.github.com>
2021-12-27 17:52:59 +0300

    added logging and exception wrapping

--- src/main/java/com/epam/xmlparsing/parser/JaxbParser.java
+++ src/main/java/com/epam/xmlparsing/parser/JaxbParser.java
@@ -2,6 +2,8 @@ package com.epam.xmlparsing.parser;
 
 import com.epam.xmlparsing.entity.Plant;
 import com.epam.xmlparsing.entity.PlantsWrapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -11,12 +13,20 @@ import java.io.FileReader;
 import java.util.List;
 
 public class JaxbParser implements Parser {
+
+    private static final Logger LOGGER = LogManager.getLogger(JaxbParser.class);
+
     @Override
-    public List<Plant> parse(String filePath) throws JAXBException, FileNotFoundException {
-        JAXBContext jc = JAXBContext.newInstance(PlantsWrapper.class);
-        Unmarshaller u = jc.createUnmarshaller();
-        FileReader reader = new FileReader(filePath);
-        PlantsWrapper plantsWrapper = (PlantsWrapper) u.unmarshal(reader);
-        return plantsWrapper.getPlants();
+    public List<Plant> parse(String filePath) throws ParserException {
+        LOGGER.info("Started parsing plants from " + filePath);
+        try {
+            JAXBContext jc = JAXBContext.newInstance(PlantsWrapper.class);
+            Unmarshaller u = jc.createUnmarshaller();
+            FileReader reader = new FileReader(filePath);
+            PlantsWrapper plantsWrapper = (PlantsWrapper) u.unmarshal(reader);
+            return plantsWrapper.getPlants();
+        } catch (JAXBException | FileNotFoundException e) {
+            throw new ParserException(e);
+        }
     }
 }
