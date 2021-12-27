bc7ce6bd7a5ff70d0f146d38e7ba9da5dabb7163
Johanot <67147458+SirJohanot@users.noreply.github.com>
2021-12-27 17:48:27 +0300

    implemented parse, added logging

--- src/main/java/com/epam/xmlparsing/parser/Director.java
+++ src/main/java/com/epam/xmlparsing/parser/Director.java
@@ -2,11 +2,18 @@ package com.epam.xmlparsing.parser;
 
 import com.epam.xmlparsing.entity.Plant;
 import com.epam.xmlparsing.validation.XMLValidator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
 import java.util.List;
 
 public class Director {
+
+    private static final Logger LOGGER = LogManager.getLogger(Director.class);
 
     private final Parser parser;
     private final XMLValidator validator;
@@ -16,7 +23,15 @@ public class Director {
         this.validator = xmlValidator;
     }
 
-    List<Plant> parse(String filePath) {
-        return null;
+    List<Plant> parse(String xmlPath, String xsdPath) throws DirectorException {
+        LOGGER.info("Started parsing plants from " + xmlPath + " while validating by " + xsdPath);
+        try {
+            if (!validator.validate(xmlPath, xsdPath)) {
+                throw new DirectorException("The xml file by specified path does not validate by the xsd file by inputted path");
+            }
+            return parser.parse(xmlPath);
+        } catch (Exception e) {
+            throw new DirectorException(e);
+        }
     }
 }
